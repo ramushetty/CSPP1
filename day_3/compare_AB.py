e35b0017ba0826de6abf7a5e1a4102b07526419d
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-01 15:45:57 +0530

    compare

---
+++ day_3/compare_AB.py
@@ -0,0 +1,10 @@
+varA=int(input())
+varB=int(input())
+if varA>varB:
+	print("varA is bigger")
+elif varA==varB:
+	print("equal") 
+elif varA<varB:
+	print("varA is smaller")
+else:
+	print("strings involved")
