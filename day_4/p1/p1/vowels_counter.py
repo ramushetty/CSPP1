3c9d777425906527fa031680f2d963e7fe3effc9
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-02 17:44:41 +0530

    " -> 4 of 4 passed. pylint: 1.11/10 "

--- day_4/p1/p1/vowels_counter.py
+++ day_4/p1/p1/vowels_counter.py
@@ -10,10 +10,10 @@ def main():
 	# remove pass and start your code here
     s = input() 
     c = 0
-    for i in range (0, len(s), 1):
+    for i in range(0, len(s), 1):
         if s[i] == 'a'or s[i] == 'e'or s[i] == 'i'or s[i] == 'o'or s[i] == 'u':
             c = c+1
     print(c)
 
-if __name__== "__main__":
+if __name__ == "__main__":
 	main()
