12f67b4e0681b492202e5de2955744ef7cea89f3
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-02 18:31:13 +0530

    " -> 0 of 5 passed. pylint: -1.00/10 "

--- day_4/p2 (1)/p2/bob_counter.py
+++ day_4/p2 (1)/p2/bob_counter.py
@@ -5,10 +5,27 @@ Write a program that prints the number of times the string 'bob' occurs in s. Fo
 Number of times bob occurs is: 2'''
 
 def main():
-	s = raw_input()
-	# the input string is in s
-	# remove pass and start your code here
-	pass
+    #s = raw_input()
+    # the input string is in s
+    # remove pass and start your code here
+    string1 = input()
+string2 = str("bob")
+l = len(string1)
+print(l)
+c = 0
+m = 0
+j = 0
+for i in range(0, l-1, 1):
+    while  string2[j] == string1[i]:
+        if c == len(string2)-1:
+            j = 0
+            m+= 1
+            c = 0
+            continue
+        c+= 1
+        j+= 1
+        
+print(m)
 
 if __name__== "__main__":
-	main()
+    main()
