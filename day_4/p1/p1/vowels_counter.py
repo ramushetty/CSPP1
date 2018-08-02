e8aa5b3f7fc91e1495bcf76bf3d2f96a69cd18c8
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-02 17:40:06 +0530

    " -> 0 of 4 passed. pylint: -1.11/10 "

--- day_4/p1/p1/vowels_counter.py
+++ day_4/p1/p1/vowels_counter.py
@@ -8,11 +8,11 @@ def main():
 	#s = raw_input()
 	# the input string is in s
 	# remove pass and start your code here
-    s= str(input("enter string") )
-    c= 0
+    s = input("enter string") 
+    c = 0
     for i in range (0, len(s), 1):
-        if s[i]== 'a'or s[i]== 'e'or s[i]== 'i'or s[i]== 'o'or s[i]== 'u':
-            c= c+1
+        if s[i] == 'a'or s[i] == 'e'or s[i] == 'i'or s[i] == 'o'or s[i] == 'u':
+            c = c+1
     print(c)
 
 if __name__== "__main__":
