67532d3701d3c63c3e3b0f4c91bd132205ecc2c8
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-02 17:56:08 +0530

    " -> 4 of 4 passed. pylint: 10.00/10 "

--- day_4/p1/p1/vowels_counter.py
+++ day_4/p1/p1/vowels_counter.py
@@ -10,12 +10,12 @@ def main():
     '''
     counting vowels
     '''
-    s = input()
-    c = 0
-    for i in range(0, len(s), 1):
-        if s[i] == 'a'or s[i] == 'e'or s[i] == 'i'or s[i] == 'o'or s[i] == 'u':
-            c = c+1
-    print(c)
+    s_i = input()
+    c_i = 0
+    for i in range(0, len(s_i), 1):
+        if s_i[i] == 'a'or s_i[i] == 'e'or s_i[i] == 'i'or s_i[i] == 'o'or s_i[i] == 'u':
+            c_i = c_i+1
+    print(c_i)
 
 if __name__ == "__main__":
     main()
