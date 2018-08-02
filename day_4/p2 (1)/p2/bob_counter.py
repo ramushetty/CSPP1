edf8b5acae0b5f3b5794548cb0a3d5af43599d2f
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-02 18:43:16 +0530

    " -> 3 of 5 passed. pylint: 9.47/10 "

--- day_4/p2 (1)/p2/bob_counter.py
+++ day_4/p2 (1)/p2/bob_counter.py
@@ -1,6 +1,8 @@
 '''Assume s is a string of lower case characters.
 
-Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
+Write a program that prints the
+ number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your p
+ rogram should print
 
 Number of times bob occurs is: 2'''
 
@@ -22,11 +24,11 @@ def main():
         while  string2[j_a] == string1[i]:
             if c_a == len(string2)-1:
                 j_a = 0
-                m_a+= 1
+                m_a += 1
                 c_a = 0
                 continue
-            c_a+= 1
-            j_a+= 1
+            c_a += 1
+            j_a += 1
         
     print(m_a)
 
