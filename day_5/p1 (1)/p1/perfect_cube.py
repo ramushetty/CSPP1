e12c169c6979e755b3c65ea538a0445995474266
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-03 15:08:41 +0530

    " -> 3 of 4 passed. pylint: -3.00/10 "

---
+++ day_5/p1 (1)/p1/perfect_cube.py
@@ -0,0 +1,26 @@
+# Write a python program to find if the given number is a perfect cube or not
+# using guess and check algorithm
+
+# testcase 1
+# Input: 24389
+# Output: 24389 is a perfect cube
+
+# testcase 2
+# Input: 21950
+# Output: 21950 is not a perfect cube
+
+def main():
+	# input is captured in s
+	s = int(input())
+	# watch out for the data type of value stored in s
+	# your code starts here
+	c_c = 0
+	while c_c**3 < s:
+		c_c = c_c+1
+	if c_c**3 != s:
+		print(str(s)+' is not a perfect cube')
+	else:
+		print(str(s)+' is a perfect cube')
+
+if __name__== "__main__":
+	main()
