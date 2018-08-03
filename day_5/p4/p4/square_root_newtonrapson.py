709babba4c101f0213b9e9acd60815e0c1d81c8b
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-03 18:08:12 +0530

    " -> 3 of 3 passed. pylint: 7.78/10 "

--- day_5/p4/p4/square_root_newtonrapson.py
+++ day_5/p4/p4/square_root_newtonrapson.py
@@ -1,4 +1,4 @@
-# Write a python program to find the square root of the given number
+'''# Write a python program to find the square root of the given number
 # using approximation method
 
 # testcase 1
@@ -7,18 +7,21 @@
 
 # testcase 2
 # input: 49
-# output: 6.999999999999991
+# output: 6.999999999999991'''
 
 def main():
-	s_s = int(input())
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	# your code starts here
-	k_k = s_s/2.0
-	while abs (k_k*k_k - s_s) >= epsilon:
-		k_k = k_k - (((k_k**2) - s_s ) / (2*k_k))
-	print(str(k_k))
+    '''
+    newton rapson
+    '''
+    s_s = int(input())
+    # epsilon and step are initialized
+    # don't change these values
+    epsilon = 0.01
+    # your code starts here
+    k_k = s_s/2.0
+    while abs (k_k*k_k - s_s) >= epsilon:
+        k_k = k_k - (((k_k**2) - s_s ) / (2*k_k))
+    print(str(k_k))
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
