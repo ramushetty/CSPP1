7b75614d8fcc916d181ec4d58171160e89779a8a
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-03 19:05:16 +0530

    " -> 3 of 3 passed. pylint: 9.09/10 "

--- day_5/p2 (3)/p2/square_root.py
+++ day_5/p2 (3)/p2/square_root.py
@@ -10,22 +10,19 @@
 # output: 6.999999999999991
 '''
 def main():
-	'''
-	square root
-	'''
-	s_s = int(input())
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
-	i_i = 0.1
-	g_g = 0
-	while abs(g_g**2 - s_s) >= epsilon:
-		g_g = g_g + i_i
-		
-
-	print(g_g)
-
-if __name__== "__main__":
-	main()
+    '''
+    square root
+    '''
+    s_s = int(input())
+    # epsilon and step are initialized
+    # don't change these values
+    epsilon = 0.01
+    step = 0.1
+    # your code starts here
+    i_i = 0.1
+    g_g = 0
+    while abs(g_g**2 - s_s) >= epsilon:
+        g_g = g_g + i_i
+    print(g_g)
+if __name__ == "__main__":
+    main()
