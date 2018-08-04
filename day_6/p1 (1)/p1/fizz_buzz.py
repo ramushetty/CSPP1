bd3db0e2c72629bade86c427f2bed00111a71d54
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-04 12:23:01 +0530

    " -> 7 of 7 passed. pylint: 9.29/10 "

--- day_6/p1 (1)/p1/fizz_buzz.py
+++ day_6/p1 (1)/p1/fizz_buzz.py
@@ -17,11 +17,9 @@ def main():
             print("Buzz")
         elif i_i%3 == 0:
             print("Fizz")
-            
         elif i_i%5 == 0:
             print("Buzz")
         else:
             print(i_i)
-        
 if __name__ == "__main__":
     main()
