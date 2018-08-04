5df8fd0e4df67a167c13799d74fb45deb03bf292
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-04 11:31:41 +0530

    " -> 7 of 7 passed. pylint: 10.00/10 "

--- day_6/p3 (2)/p3/digit_product.py
+++ day_6/p3 (2)/p3/digit_product.py
@@ -1,6 +1,6 @@
 '''
 Given a  number int_input, find the product of all the digits
-#example: 
+#example:
     input: 123
     output: 6
 '''
@@ -8,7 +8,7 @@ def main():
     '''
     Read any number from the input, store it in variable int_input.
     '''
-    int_input =(int(input()))
+    int_input = (int(input()))
     pro = 1
     if int_input == 0:
         print(0)
