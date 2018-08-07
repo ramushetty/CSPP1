f4f05e9964bbf59092a2e7694b7c21f0d117dea1
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-07 15:13:35 +0530

    " -> 9 of 10 passed. pylint: 10.00/10 "

--- day_8/p1 (4)/p1/assignment1.py
+++ day_8/p1 (4)/p1/assignment1.py
@@ -5,18 +5,18 @@
 # This function takes in one number and returns one number.
 '''
 
-def factorial(n_n):
+def factorial(_n_n):
     '''
     n is positive Integer
 
     returns: a positive integer, the factorial of n.
     '''
     # Your code here
-    if n_n == 0:
+    if _n_n == 0:
         return 1
-    if n_n == 1:
+    if _n_n == 1:
         return 1
-    return n_n*factorial(n_n-1)
+    return _n_n*factorial(_n_n-1)
 def main():
     '''
     factorial
