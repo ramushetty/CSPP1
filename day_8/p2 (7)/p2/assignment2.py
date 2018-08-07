cdba8fd279094b5b9c87b591fb02ab551cae743a
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-07 14:28:03 +0530

    " -> 10 of 10 passed. pylint: 6.67/10 "

--- day_8/p2 (7)/p2/assignment2.py
+++ day_8/p2 (7)/p2/assignment2.py
@@ -4,24 +4,23 @@ returns the sum of digits of given number.
 
 # This function takes in one number and returns one number.
 '''
-sum = 0
-def sumofdigits(n):
+def sumofdigits(n_n):
     '''
     n is positive Integer
 
     returns: a positive integer, the sum of digits of n
     '''
     # Your code here
-    if n==0:
-    	return 0
+    if n_n == 0:
+        return 0
     else:
-    	return (n%10+sumofdigits(n//10))
+        return (n_n%10+sumofdigits(n_n//10))
 def main():
-	'''
-	sum of digits
-	'''
-	a = input()
-	print(sumofdigits(int(a)))
+    '''
+    sum of digits
+    '''
+    a_a = input()
+    print(sumofdigits(int(a_a)))
 if __name__ == "__main__":
     main()
 
