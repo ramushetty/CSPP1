4a905da4a58003a4d39e9e8d68290bd9b1a58b01
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-07 14:23:36 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

---
+++ day_8/GCD using Iteration/GCD using Iteration/gcd_iter.py
@@ -0,0 +1,30 @@
+# Exercise: GCDIter
+# Write a Python function, gcdIter(a, b), that takes in two numbers and returns
+#the GCD(a,b) of given a and b.
+
+# This function takes in two numbers and returns one number.
+
+
+def gcdIter(a, b):
+    '''
+    a, b: positive integers
+    
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Your code here
+    i=0
+    while i>=0:
+    	if a%i==0 and b%i==0:
+    		c_c = i+1
+    	i=i+1
+    return c_c
+def main():
+	'''
+	gcd ofa number
+	'''
+	data = input()
+	data = data.split()
+	print(gcdIter(int(data[0]),int(data[1]))) 
+
+if __name__ == "__main__":
+    main()
