6b96a906a5b8efdf42965c206271b91e20c5e29f
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-08 14:35:10 +0530

    gcd

--- day_8/GCD using Recursion/GCD using Recursion/gcd_recr.py
+++ day_8/GCD using Recursion/GCD using Recursion/gcd_recr.py
@@ -4,7 +4,7 @@
 # This function takes in two numbers and returns one number.
 
 
-def gcdRecur(a, b, i):
+def gcdRecur(a, b):
     '''
     a, b: positive integers
     
@@ -12,20 +12,12 @@ def gcdRecur(a, b, i):
     '''
     # Your code here
     
-    if i==min(a,b):
-    	return gcd
-    if a%i==0 and b%i==0:
-    	gcd = i
-    return gcd(a,b,i+1)
-
-    
 
 
 def main():
-	i=1
-	data = input()
-	data = data.split()
-	print(gcdRecur(int(data[0]),int(data[1],i))
+    data = input()
+    data = data.split()
+    print(gcdRecur(int(data[0]),int(data[1])))
 
-if __name__ == "__main__":
+if __name__== "__main__":
     main()
