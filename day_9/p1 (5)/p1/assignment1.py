2e7533fdcbebe00fc9f3c9acff7e8873680507fd
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-09 19:42:47 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- day_9/p1 (5)/p1/assignment1.py
+++ day_9/p1 (5)/p1/assignment1.py
@@ -17,7 +17,7 @@ def is_word_guessed(secret_word, letters_guessed):
     # FILL IN YOUR CODE HERE...
     for j in secret_word:
         if j not in letters_guessed:
-            return False    
+            return False
     return True
 def main():
     '''
