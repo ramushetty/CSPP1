a48bcf0cd7f7b54c606c677e334ed4219cb79d69
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-09 19:12:14 +0530

    " -> 0 of 10 passed. pylint: 8.24/10 "

---
+++ day_10/p1 (6)/p1/assignment1.py
@@ -0,0 +1,43 @@
+'''
+Exercise : Assignment-1
+implement the function get_available_letters that takes in one parameter -
+a list of letters, letters_guessed. This function returns a string
+that is comprised of lowercase English letters - all lowercase English letters
+that are not in letters_guessed
+'''
+
+def get_available_letters(letters_guessed):
+    '''
+    :param letters_guessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
+    import string
+    st = string.ascii_lowercase
+    re = ' '
+    for i in st:
+        if i not in letters_guessed:
+            re = re +i
+    return re
+
+
+
+
+def main():
+    '''
+    Main function for the given program
+
+    '''
+    user_input = input()
+    #print(user_input)
+    user_input = user_input.split()
+    #print(user_input)
+    data = []
+    for char in user_input:
+        data.append(char[0])
+        #print(data)
+    print(get_available_letters(data))
+
+
+if __name__ == "__main__":
+    main()
