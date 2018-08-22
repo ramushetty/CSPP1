ff8e050e9c036501e063df038239e76878efa30e
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-22 23:00:42 +0530

    " -> 3 of 19 passed. pylint: 8.58/10 "

--- aug_15_poker_v2/poker.py
+++ aug_15_poker_v2/poker.py
@@ -179,8 +179,8 @@ def hand_rank(hand):
        return 3
     if is_twopair(hand):
         return 2
-    if is_onepair(hand):
-        return 1    
+    if is_onepair(hand) !=100:
+        return is_onepair(hand)
     return high_card(hand)
 def poker(hands):
     '''
