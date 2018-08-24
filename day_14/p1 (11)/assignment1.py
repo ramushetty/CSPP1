1c5e3faadd2e7a1f212a105c466f602ad7bc0553
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-24 21:31:02 +0530

    " -> 0 of 5 passed. pylint: 7.80/10 "

--- day_14/p1 (11)/assignment1.py
+++ day_14/p1 (11)/assignment1.py
@@ -88,8 +88,10 @@ class Message:
         lower_values = list(string.ascii_lowercase)
         shift_lower_values = lower_values[shift:] + lower_values[:shift]
         upper_keys = list(string.ascii_uppercase)
+        upper_values = list(string.ascii_uppercase)
         upper_shift_values = upper_values[shift:] + upper_values[:shift]
         full_keys = lower_keys + upper_keys
+
         full_values = shift_lower_values + upper_shift_values
         self.shift_dict = dict(zip(full_keys, full_values))
         return self.shift_dict
