4bb048969f42a474ffde736368a92e390e0ce9cb
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-25 11:33:19 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

---
+++ day_22/assignment3/assignment3/tokenize.py
@@ -0,0 +1,44 @@
+'''
+Write a function to tokenize a given string and return a dictionary with the frequency of
+each word
+'''
+import re
+def clean_index(index):
+	'''
+	to remove special characters
+	'''
+	listt1 = index
+	out = re.sub(r'[^a-zA-z]', ' ', listt1).split()
+	return out
+
+def tokenize(string):
+	'''
+	token size
+	'''
+	listt = string
+	dictt = {}
+	for index in listt:
+		#print(index)
+		clean = clean_index(index)
+		for j in clean:
+			if j in dictt:
+				dictt[j] += 1
+			else:
+				dictt[j] = 1
+	return dictt
+
+
+
+            
+def main():
+	'''
+	main
+	'''
+    intt = int(input())
+    doc = []
+    for i in range(intt):
+    	doc.append(input())
+    #print(doc)
+    print(tokenize(doc))
+if __name__ == '__main__':
+    main()
