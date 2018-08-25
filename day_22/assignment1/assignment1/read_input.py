f6b5a4706d1eafaa2f56a490a579bda4d7346ccd
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-25 10:44:18 +0530

    " -> 2 of 2 passed. pylint: 6.36/10 "

---
+++ day_22/assignment1/assignment1/read_input.py
@@ -0,0 +1,19 @@
+'''
+Write a python program to read multiple lines of text input and store the input into a string.
+'''
+
+def main():
+    inputt = int(input())
+    stringg = []
+    e_m = ''
+    for i in range(inputt):
+    	stringg.append(list(map(str, input().split('\n'))))
+
+    for i in range(inputt):
+    	#print(stringg)
+    	e_m = ''.join(stringg[i])
+    	print(e_m)
+
+
+if __name__ == '__main__':
+    main()
