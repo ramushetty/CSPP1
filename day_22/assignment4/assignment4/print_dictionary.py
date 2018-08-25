e33fc1f2cb239d85b90b0801adbfd7ddd7c44b2d
ramushetty <ummadishettyramu@msitprogram.net>
2018-08-25 11:51:02 +0530

    " -> 0 of 3 passed. pylint: 7.00/10 "

--- day_22/assignment4/assignment4/print_dictionary.py
+++ day_22/assignment4/assignment4/print_dictionary.py
@@ -5,19 +5,19 @@ the key and frequency with a SPACE - SPACE.
 '''
 
 def print_dictionary(dictionary):
-	'''
-	print output with key and values
-	'''
+    '''
+    print output with key and values
+    '''
     out = dictionary
     lenn = len(dictionary)
     for key,value in out.items():
-    	#print(key)
-    	print(key+' '+'-'+' '+str(value))
+        #print(key)
+        print(key+' '+'-'+' '+str(value))
 
 def main():
-	'''
-	main function
-	'''
+    '''
+    main function
+    '''
     dictionary = eval(input())
     print_dictionary(dictionary)
 
