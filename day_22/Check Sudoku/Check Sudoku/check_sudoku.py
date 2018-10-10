5863038fc38a001a43f7934bc6bbbbf8a010b126
ramushetty <ummadishettyramu@msitprogram.net>
2018-10-10 16:00:18 +0530

    " -> 26 of 26 passed. pylint: 8.68/10 "

--- day_22/Check Sudoku/Check Sudoku/check_sudoku.py
+++ day_22/Check Sudoku/Check Sudoku/check_sudoku.py
@@ -15,28 +15,50 @@ def check_sudoku(sudoku):
         The function has to return True for a valid sudoku grid and false otherwise
     '''
     #print(sudoku)
-    for i in sudoku:
-        if i != 9:
-            return False
-    mat1 = sudoku
-    x_x = len(sudoku)
-    for i in range(x_x):
-        for j in range(len(mat1[i])):
-            # if i == 9:
-            #     if mat1[i][j] == mat1[i][j]:
-            #         return False
-            # if j == 9:
-            #     if mat1[i][j] == mat1[i][j]:
-            #         return False
-            if i == j:
-                if mat1[i][j] == mat1[i][j]:
-                    return False
-            # if i+j == i + j:
-            #     if mat1[i][j] == mat1[i][j]:
-            #         return False
-            # if i-j == i - j:
-            #     if mat1[i][j] == mat1[i][j]:
-            #         return False
+    c = 0
+    for row in sudoku:
+        if len(set(row)) == 9 and int(max(row))-int(min(row)) == 8:
+            c += 1
+    for out in range(0, 9):
+        mat1 = []
+        for inside in range(0, 9):
+            mat1.append(sudoku[inside][out])
+        if len(set(mat1)) == 9 and int(max(mat1))-int(min(mat1)) == 8:
+            c += 1
+    for _ in range(0, 9):
+        mat1 = []
+        mat2 = []
+        mat3 = []
+        for out in range(0, 3):
+            for inside in range(0, 3):
+                mat1.append(sudoku[out][inside])
+            for inside in range(3, 4):
+                mat2.append(sudoku[out][inside])
+            for inside in range(4, 9):
+                mat3.append(sudoku[out][inside])
+        for out in range(3, 4):
+            for inside in range(0, 3):
+                mat1.append(sudoku[out][inside])
+            for inside in range(3, 4):
+                mat2.append(sudoku[out][inside])
+            for inside in range(4, 9):
+                mat3.append(sudoku[out][inside])
+        for out in range(4, 9):
+            for inside in range(0, 3):
+                mat1.append(sudoku[out][inside])
+            for inside in range(3, 4):
+                mat2.append(sudoku[out][inside])
+            for inside in range(4, 9):
+                mat3.append(sudoku[out][inside])
+        if len(set(mat1)) == 9 and int(max(mat1))-int(min(mat1)) == 8:
+            c += 1
+        if len(set(mat2)) == 9 and int(max(mat2))-int(min(mat2)) == 8:
+            c += 1
+        if len(set(mat3)) == 9 and int(max(mat3))-int(min(mat3)) == 8:
+            c += 1
+    if c == 45:
+        return True
+    return False
 def main():
     '''
         main function to read input sudoku from console
